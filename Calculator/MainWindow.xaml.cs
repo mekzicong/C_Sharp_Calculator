c505abcadcbb1e8e5e3eb4b5e2afa86e237e03f3
mekzicong@gmail.com <mekzicong@gmail.com>
2020-10-15 13:27:39 +0800

    new updates

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,21 +20,175 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastNumber, result;//stored the last number display on the calculator
+        SelectedOperator selectedOperator;
         public MainWindow()
         {
             InitializeComponent();
-            resultLabel.Content = "1234";
+
+            acButton.Click += AcButton_Click;// same as in xaml used Click:"acButton_Click"
+            negativeButton.Click += NegativeButton_Click;
+            percentageButton.Click += PercentageButton_Click;
+            equalButton.Click += EqualButton_Click;
         }
 
-        private void sevenButton_Click(object sender, RoutedEventArgs e)
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
         {
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.Add(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Subtration:
+                        result = SimpleMath.Subtrate(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Multiple(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.Division(lastNumber, newNumber);
+                        break;
+                }
+                resultLabel.Content = result.ToString();
+            }
+
+        }
+
+        private void PercentageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber / 100;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+        // as we have many number button,it is impratical to create repetively same method
+        //private void sevenButton_Click(object sender, RoutedEventArgs e) 
+        //{
+        //    if (resultLabel.Content.ToString() == "0")
+        //    {
+        //        resultLabel.Content = "7";
+        //    }
+        //    else
+        //    {
+        //        resultLabel.Content = $"{resultLabel.Content}7";
+        //    }
+        //}
+        // Create a method by assigning all button with same click method
+        //sender is the on trigerring the event handler
+        // sender is equal to x:Name=""
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedValue = 0;
+            if (sender == zeroButton)
+                selectedValue = 0;
+            if (sender == oneButton)
+                selectedValue = 1;
+            if (sender == twoButton)
+                selectedValue = 2;
+            if (sender == threeButton)
+                selectedValue = 3;
+            if (sender == fourButton)
+                selectedValue = 4;
+            if (sender == fiveButton)
+                selectedValue = 5;
+            if (sender == sixButton)
+                selectedValue = 6;
+            if (sender == sevenButton)
+                selectedValue = 7;
+            if (sender == eightButton)
+                selectedValue = 8;
+            if (sender == nineButton)
+                selectedValue = 9;
+
             if (resultLabel.Content.ToString() == "0")
             {
-                resultLabel.Content = "0";
+                resultLabel.Content = $"{selectedValue}";
             }
             else
             {
-                resultLabel.Content = $"{resultLabel.Content}7";
+                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+            }
+        }
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+                //store the dispaly value to the variable then clear the display, and store the opearation
+            {
+                resultLabel.Content = "0";
+            }
+            if (sender == multiplyButton)
+            {
+                selectedOperator = SelectedOperator.Multiplication;
+            }
+            if (sender == divideButton)
+            {
+                selectedOperator = SelectedOperator.Division;
+            }
+            if(sender==plusButton)
+            {
+                selectedOperator = SelectedOperator.Addition;
+            }
+            if (sender == minusButton)
+            {
+                selectedOperator = SelectedOperator.Subtration;
+            }
+        }
+        public enum SelectedOperator
+        {
+            Addition,
+            Subtration,
+            Multiplication,
+            Division
+
+        }
+
+        private void pointButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Contains("."))
+            {
+
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}.";
+            }
+        }
+
+        public class SimpleMath
+        {
+            public static double Add(double n1,double n2)
+            {
+                return n1 + n2;
+            }
+            public static double Subtrate(double n1, double n2)
+            {
+                return n1 - n2;
+            }
+            public static double Multiple(double n1, double n2)
+            {
+                return n1*n2;
+            }
+            public static double Division(double n1, double n2)
+            {
+                return n1 / n2;
             }
         }
     }
