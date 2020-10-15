1aef1a06bc1ba824f4749de37778f74c2954f075
mekzicong@gmail.com <mekzicong@gmail.com>
2020-10-15 11:41:34 +0800

    further updates

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -23,6 +23,19 @@ namespace Calculator
         public MainWindow()
         {
             InitializeComponent();
+            resultLabel.Content = "1234";
+        }
+
+        private void sevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "0";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}7";
+            }
         }
     }
 }
